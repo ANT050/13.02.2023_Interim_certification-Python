a00ca0bff9bdf5d9be2d30176130a6ffd85ad66e
Anton <tepa-86@mail.ru>
2023-02-12 21:11:51 +0300

    Редактирование файла README: - Добавление в конце текста относительной ссылки - Изменение шрифта условия задачи

--- controller.py
+++ controller.py
@@ -2,10 +2,12 @@ import view
 import model
 
 def main():
+    #Открытие главного меню, выбор пункта меню
     view.open_main_menu()
     main_menu_choice = view.get_user_choice()
     model.handle_main_menu_choice(main_menu_choice)
 
+    #Открытие основного меню пользователю, выбор пункта меня
     view.display_notes_menu()
     notes_menu_choice = view.user_choice_menu_item()
     model.selecting_note_menu_item(notes_menu_choice)
