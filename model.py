728582274d2f9f6c1e4f008fbb31b386bfff6331
Anton <tepa-86@mail.ru>
2023-02-13 06:01:08 +0300

    Изменение файла model: - в функции open_all_notes добавлен функционал вывода списка заметок в виде таблице,    если поля не заполнены, то выводятся пустые поля,    если число символов в поле превышает 30, то идет перенос на другую строку

--- model.py
+++ model.py
@@ -1,28 +1,56 @@
+from tabulate import tabulate
 import csv
-#Функция открытия всех заметок
-def open_all_notes():
-    with open('notes.csv', 'r', encoding='UTF-8') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            print(" - ".join(row).replace(";", " "))
+import time
 
-#Функция добавления заметок
+# Функция открытия всех заметок
+
+
+def open_all_notes():
+    notes = []
+    with open('notes.csv', 'r', encoding='UTF-8') as file:
+        reader = csv.reader(file, delimiter=';')
+        for row in reader:
+            try:
+                title = row[1][:30] + '\n' + \
+                    row[1][30:] if len(row[1]) > 30 else row[1]
+                body = row[2][:30] + '\n' + \
+                    row[2][30:] if len(row[2]) > 30 else row[2]
+                creation_time = row[3] if row[3] else ''
+                change_time = row[4] if row[4] else ''
+                notes.append([row[0], title, body, creation_time, change_time])
+            except IndexError:
+                notes.append(['', '', '', '', ''])
+    print(tabulate(notes, headers=['\033[91mID\033[0m', '\033[91mЗаголовок\033[0m', '\033[91mОписание заметки\033[0m',
+          '\033[91mДата/время создания\033[0m', '\033[91mДата/время изменения\033[0m'], tablefmt="fancy_grid", stralign="center"))
+
+
+# Функция добавления заметок
 def adding_notes():
     pass
 
-#Функция редактирования заметок
+# Функция редактирования заметок
+
+
 def editing_notes():
     pass
 
-#Функция удаления заметок
+# Функция удаления заметок
+
+
 def deleting_notes():
     pass
 
-#Функция выборки заметок по дате добавления
+# Функция выборки заметок по дате добавления
+
+
 def selection_of_notes_by_date():
     pass
 
-#Функция обработки открытия основного меню
+# Функция обработки открытия основного меню
+
+
 def handle_main_menu_choice(choice):
     if choice == 2:
+        print("--"*55 + "\n" +
+              "\033[35mВы завершили работу в приложении!!!\033[0m")
         exit()
